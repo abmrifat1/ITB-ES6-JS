cba1503f66785e64cc757d7de49532f786d5ba4e
abmrifat1 <rifatabm1@gmail.com>
2019-09-23 17:43:40 +0600

    array related

--- day8.js
+++ day8.js
@@ -1,8 +1,37 @@
-var timesTwo = params => {
-     params += 2;
-     var obj={
-         
-     }
+
+
+
+  var obj = {
+    id: 48,
+    counter: function counter() {
+      setTimeout(() => {
+        console.log(this.id);
+      }, 1000);
+    }
+  };
+
+  console.log(obj.counter())
+
+
+//console.log (obj.address);
+
+
+
+//find lenght of the largest word in  string;
+
+var sentence="my name is khorshed alam mazumder";
+function findLargestLength(str){
+  var str=sentence.split(' ');
+  var maxlenght=0;
+  for(var i=0;i<=(str.length)-1;i++){
+    if(str[i].length>=maxlenght){
+      maxlenght=str[i].length;
+    }
+  }
+  return maxlenght;
 }
 
-console.log (timesTwo(4)); 
+console.log(findLargestLength(sentence))
+
+
+
